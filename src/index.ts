4e94638c461ff2562b71b969ce07e704fc55c995
Shayon <7rat13@gmail.com>
2025-03-20 12:32:11 +0100

    Remove completions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { parser } from "./tql.grammar";
 import { LRLanguage, LanguageSupport } from "@codemirror/language";
 import { styleTags, tags as t } from "@lezer/highlight";
-import { completeFromList, Completion } from "@codemirror/autocomplete";
-import { data } from "../output.js";
 
 const punctuation = `"+" "-" "*" "/" "=" "." "'" ":" "!" "?" "<" ">" "@" "%" "&" "#" ";" "^" "\`"`;
 
@@ -25,40 +23,6 @@ export const TenzirQueryLang = LRLanguage.define({
   }),
 });
 
-type GeneretedCompletion = {
-  label: string;
-  type: string;
-  detail: string;
-  processedHTML: string;
-  docLink: string;
-};
-
-const getCompletion = (completion: GeneretedCompletion): Completion => {
-  return {
-    label: completion.label,
-    type: completion.type,
-    detail: completion.detail,
-    info: () => {
-      const node = document.createElement("div");
-      const div = document.createElement("div");
-      div.className = "custom-completioninfoHeader";
-
-      div.innerHTML = `<div><a class="custom-docLink" href="${completion.docLink}" target="_blank"><span>${completion.label}</span><div class="custom-icon"></div></a></div>`;
-
-      node.insertAdjacentElement("afterbegin", div);
-      node.innerHTML += completion.processedHTML;
-      return node;
-    },
-  };
-};
-const tqlCompletionList = data.map((completion: GeneretedCompletion) =>
-  getCompletion(completion),
-);
-
-export const tqlCompletion = TenzirQueryLang.data.of({
-  autocomplete: completeFromList(tqlCompletionList),
-});
-
 export function TQL() {
-  return new LanguageSupport(TenzirQueryLang, [tqlCompletion]);
+  return new LanguageSupport(TenzirQueryLang);
 }
